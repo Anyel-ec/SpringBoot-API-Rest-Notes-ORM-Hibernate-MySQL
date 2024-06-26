3b6cb452d6f9acf41f73274f86e3898e3184811b
anyel-ec <paulangel826@gmail.com>
2024-06-26 09:31:48 -0500

    start project in spring boot 3.3.1

---
+++ src/main/java/top/anyel/hibernate/SpringBootHibernateOrmApplication.java
@@ -0,0 +1,13 @@
+package top.anyel.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHibernateOrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHibernateOrmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/anyel/hibernate/SpringBootHibernateOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package top.anyel.hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHibernateOrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
